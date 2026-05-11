d735ea21ba815483f5cbf3cb0c8e9dd9b83773c6
jpg-ms-yt <tinovalegomez@gmail.com>
2026-05-11 09:20:15 -0500

    Add files via upload

---
+++ Programa28SelectorColor.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+public class Programa28SelectorColor extends JFrame {
+ public Programa28SelectorColor() {
+ setTitle("Selector de color");
+ setSize(350, 200);
+ setLocationRelativeTo(null);
+ setDefaultCloseOperation(EXIT_ON_CLOSE);
+ JButton btn = new JButton("Elegir color");
+ btn.addActionListener(e -> {
+ Color c = JColorChooser.showDialog(this, "Seleccione color", Color.WHITE);
+ if (c != null) getContentPane().setBackground(c);
+ });
+ add(btn);
+ setVisible(true);
+ }
